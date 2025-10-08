1a34ad24512d71f50b194ff5b9ff1fea386fda0d
haydenphancs <haydenphancs@gmail.com>
2025-10-08 09:01:50 -0600

    change the name database in init_db

--- init_db.py
+++ init_db.py
@@ -2,16 +2,16 @@
 import sqlite3
 
 DB_NAME = "risk_assessment.db"
-SCHEMA_FILE = "borroDB.sql"
+borroDB_FILE = "borroDB.sql"
 
-# Connect to the database (this creates the file if it doesn't exist)
+# connect to the database 
 connection = sqlite3.connect(DB_NAME)
 
 # Enable foreign key support
 connection.execute("PRAGMA foreign_keys = 1")
 #
 # Read and execute the schema file
-with open(SCHEMA_FILE) as f:
+with open(borroDB_FILE) as f:
     connection.executescript(f.read())
 
 print(f"Database '{DB_NAME}' created successfully.")
