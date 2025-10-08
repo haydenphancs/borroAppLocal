c0aa847144ee3d8c27926866393bc5f44475e7b3
haydenphancs <haydenphancs@gmail.com>
2025-10-07 22:31:13 -0600

    change the name DB

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 DB_NAME = "risk_assessment.db"
-SCHEMA_FILE = "schema.sql"
+SCHEMA_FILE = "borroDB.sql"
 
 # Connect to the database (this creates the file if it doesn't exist)
 connection = sqlite3.connect(DB_NAME)
